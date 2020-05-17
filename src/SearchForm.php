f1ddeaba0a981f8f0bee11dc2186137e58900eed
wpsmith <t@wpsmith.net>
2020-05-17 16:50:22 -0400

    Search form PHP docs

--- src/SearchForm.php
+++ src/SearchForm.php
@@ -1,4 +1,24 @@
 <?php
+/**
+ * The [search_form] shortcode.
+ *
+ * Example use: [search_form]
+ *
+ * You may copy, distribute and modify the software as long as you track
+ * changes/dates in source files. Any modifications to or software including
+ * (via compiler) GPL-licensed code must also be made available under the GPL
+ * along with build & install instructions.
+ *
+ * PHP Version 7.2
+ *
+ * @category   WPS\WP\Shortcodes
+ * @package    WPS\WP\Shortcodes
+ * @author     Travis Smith <t@wpsmith.net>
+ * @copyright  2020 Travis Smith
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
+ * @link       https://wpsmith.net/
+ * @since      0.0.1
+ */
 
 namespace WPS\WP\Shortcodes;
 
